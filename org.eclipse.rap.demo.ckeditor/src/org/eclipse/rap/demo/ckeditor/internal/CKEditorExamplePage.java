39f148d01028a34cbdcebff364796c841a144820
Tim Buschtoens <tbuschto@eclipsesource.com>
2012-03-20 16:48:41 +0100

    cosmetic UI changes

--- org.eclipse.rap.demo.ckeditor/src/org/eclipse/rap/demo/ckeditor/internal/CKEditorExamplePage.java
+++ org.eclipse.rap.demo.ckeditor/src/org/eclipse/rap/demo/ckeditor/internal/CKEditorExamplePage.java
@@ -15,7 +15,10 @@ import org.eclipse.rap.examples.IExamplePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.ToolBar;
@@ -36,29 +39,29 @@ public class CKEditorExamplePage implements IExamplePage {
     Composite composite = new Composite( parent, SWT.NONE );
     composite.setLayoutData( ExampleUtil.createFillData() );
     composite.setLayout( ExampleUtil.createGridLayout( 1, true, true, true ) );
-    //ExampleUtil.createHeading( parent, "Rich Text Editor", 1 );
     final CKEditor ckEditor = new CKEditor( composite, SWT.NONE );
     ckEditor.setText( "<b>Rich</b> <i>Text</i> <u>Editor</u>" );
     GridData layoutData = new GridData( SWT.FILL, SWT.FILL, true, true  );
     ckEditor.setLayoutData( layoutData );
-    //ckEditor.setBackground( display.getSystemColor( SWT.COLOR_YELLOW ) );
-    ToolBar toolbar = new ToolBar( composite, SWT.FLAT );
+    ckEditor.setBackground( new Color( display, 247, 247, 247 ) );
+    Composite toolbar = new Composite( composite, SWT.NONE );
     toolbar.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false  ) );
-    ToolItem printBtn = new ToolItem( toolbar, SWT.PUSH );
+    toolbar.setLayout( new GridLayout( 3, false ) );
+    Button printBtn = new Button( toolbar, SWT.PUSH );
     printBtn.setText( "Show Content" );
     printBtn.addSelectionListener( new SelectionAdapter() {
       public void widgetSelected( SelectionEvent e ) {
         //System.out.println( ckEditor.getText() );
       }
     } );
-    ToolItem fontBtn = new ToolItem( toolbar, SWT.PUSH );
+    Button fontBtn = new Button( toolbar, SWT.PUSH );
     fontBtn.setText( "Font" );
     fontBtn.addSelectionListener( new SelectionAdapter() {
       public void widgetSelected( SelectionEvent e ) {
         ckEditor.setFont( new org.eclipse.swt.graphics.Font( display, "serif", 13, 0 ) );
       }
     } );
-    ToolItem clearBtn = new ToolItem( toolbar, SWT.NONE );
+    Button clearBtn = new Button( toolbar, SWT.NONE );
     clearBtn.setText( "Clear" );
     clearBtn.addSelectionListener( new SelectionAdapter() {
       public void widgetSelected( SelectionEvent e ) {
